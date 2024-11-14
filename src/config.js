5e7cccdc1f3e483cdd3dd06d8221ac55c425da4e
0xAlvi <alviaptianpr96@gmail.com>
2024-11-14 17:37:42 +0700

    fix: linter

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ class Config {
   constructor() {
     this.baseURL = 'https://nodepay.org';
     this.ipCheckURL = 'https://ipinfo.io/json';
-    this.pingURL = 'http://13.215.134.222/api/network/ping';
+    this.pingURL = 'https://nw.nodepay.org/api/network/ping';
     this.retryInterval = 30000;
     this.sessionURL = 'http://18.136.143.169/api/auth/session';
   }
